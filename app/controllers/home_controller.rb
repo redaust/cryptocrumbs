02807487307ba2b549b645f4aef4e36d38f80a87
Austin Marsh <redausty@live.com>
2023-07-02 22:20:00 -0400

    adding price data API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,11 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
 
-# Grab Price Data
-    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD'
-    @prices_uri = URI(@url)
-    @prices_response = Net::HTTP.get(@uri)
-    @prices = JSON.parse(@response)
+      # Grab Price Data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,LTC&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
   end
 
 
