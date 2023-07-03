63d7a73b9833994487118e47e983e6ed1fe13e31
Austin Marsh <redausty@live.com>
2023-07-02 21:47:08 -0400

    added API linkage and news dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,17 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-
+# Grab News
     @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
-    
+
+# Grab Price Data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD'
+    @prices_uri = URI(@url)
+    @prices_response = Net::HTTP.get(@uri)
+    @prices = JSON.parse(@response)
   end
 
 
