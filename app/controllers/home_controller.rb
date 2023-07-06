5bd1e8e73fe98b3d2a39bf2ab90915514d63a2c5
Austin Marsh <redausty@live.com>
2023-07-05 22:04:54 -0400

    adding error handling for lookup form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,17 +17,20 @@ class HomeController < ApplicationController
 
 
   def prices
-    @symbol = params[:sym]
-    @symbol = @symbol.upcase
-
 
     require 'net/http'
     require 'json'
+    @symbol = params[:sym]
 
+    if @symbol
+    @symbol = @symbol.upcase
     @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD'
     @quote_uri = URI(@quote_url)
     @quote_response = Net::HTTP.get(@quote_uri)
     @quote = JSON.parse(@quote_response)
   end
-
 end
+end
+
+
+
